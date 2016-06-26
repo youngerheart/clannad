6a5f3da54d2c25effd74614f6e5e91a1936f5d06
youngerheart <435495400@qq.com>
2016-06-26 18:09:39 +0800

    统一接口报错

--- controllers/project.js
+++ controllers/project.js
@@ -1,25 +1,23 @@
 import Project from '../schemas/project';
+import RestError from '../services/resterror';
 
 export default {
   async add(ctx) {
     var {project: projectName, domains} = ctx.req.body;
-    try {
-      var project = await Project.findOne({name: projectName});
-      if (project) throw {
-        code: 'PROJECT_EXIST_ERR',
-        message: 'that project is exist.'
-      }
-      project = new Project({name: projectName, domains});
-      await project.save();
-      ctx.status = 200;
-      ctx.body = project._id;
-    } catch (err) {
-      console.log(err);
-      ctx.status = 400;
-      ctx.body = err;
+    var project = await Project.findOne({name: projectName});
+    if (project) throw new RestError(400, 'PROJECT_EXIST_ERR', `project ${projectName} is exist`);
+    project = new Project({name: projectName, domains});
+    await project.save();
+    ctx.status = 200;
+    ctx.body = project._id;
+  },
+  async del(ctx) {
+    var {project: projectName} = ctx.params;
+    var project = await Project.findOne({name: projectName});
+    if (!project) {
+      throw new RestError(404, 'PROJECT_NOTFOUND_ERR', `project ${projectName} is not found`);
     }
   },
-  async del(ctx) {},
   async edit(ctx) {},
   async list(ctx) {},
   async count(ctx) {},
